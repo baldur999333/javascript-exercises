83acc6888a7f47ca2e1d1304d635fb8dfeeab608
ragnarok <246638660+baldur999333@users.noreply.github.com>
2025-12-10 13:15:23 +0200

    Painstakingly do 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const filtered = array.filter(item => !args.includes(item));
+    console.log(filtered);
+    return filtered;
 };
+removeFromArray([1, 2, 2, 3, 4, 5], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
