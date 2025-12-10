13ea354fd9b1a06bbec0d73231c3c431d7257ed0
ragnarok <246638660+baldur999333@users.noreply.github.com>
2025-12-10 11:16:59 +0200

    Do 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text, amount) {
+    if(amount < 0)
+        return "ERROR";
+    let result = ""
+    for(let i = 0; i < amount; ++i){
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
