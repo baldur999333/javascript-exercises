ff78c2c15ffcd7e3c05017178c6ec5bc5b4e5967
ragnarok <246638660+baldur999333@users.noreply.github.com>
2025-12-10 13:30:12 +0200

    Do 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0)
+        return "ERROR";
+    if(!(Number.isInteger(a) && Number.isInteger(b)))
+        return "ERROR";
+    if(a == b)
+        return a;
+    if(a > b){
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    let result = 0;
+    for(let i = a; i <= b; ++i){
+        result += i;
+    }
+    return result;
 
 };
 
