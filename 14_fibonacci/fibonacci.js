7fad42179be0b0fd0ad39ba8f365919b5381f249
ragnarok <246638660+baldur999333@users.noreply.github.com>
2025-12-15 08:32:53 +0200

    Do 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num, prev = 0, cur = 1) {
+    num = +num;
+    if(num < 0 || num === NaN)
+        return "OOPS";
+    if(num === 0)
+        return prev;
+    let temp = cur + prev;
+    prev = cur;
+    cur = temp;
+    return fibonacci (--num, prev, cur);
+
+    
+
 
 };
+// console.log(fibonacci(25));
 
 // Do not edit below this line
 module.exports = fibonacci;
