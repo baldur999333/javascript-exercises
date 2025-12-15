9e744bda3e48ff10f08a4821a7058dd8364bc6d6
ragnarok <246638660+baldur999333@users.noreply.github.com>
2025-12-15 08:13:25 +0200

    Do 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLocaleLowerCase();
+    let re = /[^\w]/g;
+    string = string.replace(re, '');
+    string = string.split('');
+    let reverse = string.slice();
+    reverse.reverse();
 
+    return reverse.join() == string.join();
 };
+
+// console.log(palindromes('zzzA car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
