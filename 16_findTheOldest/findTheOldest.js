d159aada1f51f160ab143f8399eea5686c5cf0c7
ragnarok <246638660+baldur999333@users.noreply.github.com>
2025-12-15 08:53:39 +0200

    Do 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((max, current) => {
+        if(!('yearOfDeath' in max)){
+            const d = new Date();
+            max.yearOfDeath = d.getFullYear();
+        }
+        if(!('yearOfDeath' in current)){
+            const d = new Date();
+            current.yearOfDeath = d.getFullYear();
+        }
+        let maxAge = max.yearOfDeath-max.yearOfBirth;
+        let currentAge = current.yearOfDeath-current.yearOfBirth;
+        return (currentAge > maxAge) ? current : max;
+    });
+    return oldest;
 
 };
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
